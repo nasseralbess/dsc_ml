ed75d427191ed7e20d3e08dc9e96dc41b3cf05f8
Khaled I Abu Qutish <abuqutishkhaled@gmail.com>
2024-12-19 16:07:01 +0300

    try this

--- linear_reg.h
+++ linear_reg.h
@@ -6,7 +6,7 @@
 class linear_reg{
     public:
         linear_reg(mat X, vectr y);
-        void fit(double alpha_input= 0.001, int iters_input = 1000000);
+        void fit(double alpha_input= 0.005, int iters_input = 500000);
         vectr predict(mat& X);
         double accuracy(const mat& X, const vectr& y);
         double mse(const mat& X, const vectr& y);
